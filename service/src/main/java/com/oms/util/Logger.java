51782730e3a549788efdcb7fb9f4ed60e39fc320
tublian-ai-engineer <nilanjan+tos@tublian.com>
2024-09-16 01:54:15 +0000

    fix: updating solution for 3

--- service/src/main/java/com/oms/util/Logger.java
+++ service/src/main/java/com/oms/util/Logger.java
@@ -1,6 +1,7 @@
 
 package com.oms.util;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -8,6 +9,10 @@ import java.io.PrintWriter;
 public class Logger {
     private String path;
     private FileOutputStream os;
+
+    public Logger() {
+        this.path = "oms.log";
+    }
 
     public void setPath(String path) {
         this.path = path;
@@ -17,16 +22,17 @@ public class Logger {
         try {
             if (os == null) {
                 if (path == null || !new java.io.File(path).canWrite()) {
-                    path = "default-log.txt";
+                    path = "oms.log";
                 }
                 os = new FileOutputStream(path, true);
             }
             PrintWriter pw = new PrintWriter(os);
-            pw.println(msg);
+            pw.println("[" + this.getClass().getName() + "] " + msg);
             pw.flush();
             os.flush();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.err.println("IOException occurred: " + e.getMessage());
+            e.printStackTrace(System.err);
         }
     }
 }
