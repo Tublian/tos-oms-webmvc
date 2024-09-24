833988bf4d744d1e61dda84827ede03364e13f7b
tublian-ai-engineer <nilanjan+tos@tublian.com>
2024-09-23 23:52:44 -0400

    fix: proposing solution for 7

--- service/src/main/java/com/oms/service/ProductService.java
+++ service/src/main/java/com/oms/service/ProductService.java
@@ -6,12 +6,11 @@ import org.springframework.stereotype.Service;
 import com.oms.repository.OrderLineRepository;
 import com.oms.repository.ProductRepository;
 import com.oms.repository.InventoryRepository;
-import com.oms.model.Product;
-import com.oms.model.Inventory;
-import com.oms.model.OrderLine;
-import com.oms.model.LineCharges;
+import com.oms.entity.Product;
+import com.oms.entity.Inventory;
+import com.oms.entity.OrderLine;
+import com.oms.entity.LineCharges;
 import org.slf4j.Logger;
-
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -34,44 +33,23 @@ public class ProductService {
     @Autowired
     Logger logger;
 
-    // Removed ShippingService declaration
-    // ShippingService shippingService;
-
-    // Removed getShippingCosts method
-    // public Shipping getShippingCosts(String pid) {
-    //     Shipping mockShipping = new Shipping();
-    //     mockShipping.setStandardShipping(0.0);
-    //     mockShipping.setExpressShipping(0.0);
-    //     return mockShipping;
-    // }
-
     public void setLogger(Logger logger) {
         this.logger = logger;
     }
 
-    // Removed getShippingService method
-    // public ShippingService getShippingService() {
-    //     return shippingService;
-    // }
-
-    // Removed setShippingService method
-    // public void setShippingService(ShippingService shippingService) {
-    //     this.shippingService = shippingService;
-    // }
-
     public Product getProductById(String pid) {
-        logger.log(this.getClass().getName());
+        logger.info(this.getClass().getName());
         Optional<Product> findById = productRepository.findById(pid);
         return findById.orElse(null);
     }
 
     public Inventory getProductInventory(String pid) {
-        logger.log(this.getClass().getName());
+        logger.info(this.getClass().getName());
         return inventoryService.fetchInventory(pid);
     }
 
     public List<Inventory> getInventoriesDescribedWith(String text) {
-        logger.log(this.getClass().getName());
+        logger.info(this.getClass().getName());
         List<Inventory> inventoryList = new LinkedList<>();
         List<Product> products = getProductsDescribedWith(text);
         for (Product p : products) {
@@ -81,12 +59,12 @@ public class ProductService {
     }
 
     public List<OrderLine> getOrderLinesForProduct(String pid) {
-        logger.log(this.getClass().getName());
+        logger.info(this.getClass().getName());
         return orderLineRepository.findByCustomerSKU(pid);
     }
 
     public Double getTotalChargesForProduct(String pid) {
-        logger.log(this.getClass().getName());
+        logger.info(this.getClass().getName());
         double res = 0;
         List<OrderLine> orderLines = getOrderLinesForProduct(pid);
         for (OrderLine orderLine : orderLines) {
@@ -97,23 +75,23 @@ public class ProductService {
     }
 
     public List<Product> getAllProducts() {
-        logger.log(this.getClass().getName());
+        logger.info(this.getClass().getName());
         return productRepository.findAll();
     }
 
     public Product getProductByName(String name) {
-        logger.log(this.getClass().getName());
+        logger.info(this.getClass().getName());
         Optional<Product> findByName = productRepository.findByName(name);
         return findByName.orElse(null);
     }
 
     public List<Product> getProductsDescribedWith(String text) {
-        logger.log(this.getClass().getName());
+        logger.info(this.getClass().getName());
         return productRepository.findByDescriptionContainingIgnoreCase(text);
     }
 
     public Product registerProduct(Product product) {
-        logger.log(this.getClass().getName());
+        logger.info(this.getClass().getName());
         Product response = productRepository.save(product);
         return response;
     }
