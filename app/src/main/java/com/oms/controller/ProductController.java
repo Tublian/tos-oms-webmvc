5cd8e24583c2e4b342ea4837c9bded02251ad1f7
tublian-ai-engineer <nilanjan+tos@tublian.com>
2024-09-25 10:37:02 -0400

    fix: proposing solution for 7

--- app/src/main/java/com/oms/controller/ProductController.java
+++ app/src/main/java/com/oms/controller/ProductController.java
@@ -1,4 +1,3 @@
-
 package com.oms.controller;
 
 import com.oms.entity.Inventory;
@@ -15,85 +14,100 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
-    private final ProductService productService;
-    private final Logger logger;
+    @Autowired
+    ProductService productService;
 
-    public ProductController(ProductService productService, Logger logger) {
-        this.productService = productService;
-        this.logger = logger;
-    }
+    @Autowired
+    Logger logger;
 
-    @GetMapping("/{productId}")
+    @GetMapping
+    @RequestMapping("/{productId}")
     public Product getProductById(@PathVariable String productId) {
         logger.log(this.getClass().getName());
         return productService.getProductById(productId);
     }
 
-    @GetMapping("/inv/{productId}")
+    @GetMapping
+    @RequestMapping("/inv/{productId}")
     public Inventory getInventoryForProduct(@PathVariable String productId) {
         logger.log(this.getClass().getName());
         return productService.getProductInventory(productId);
     }
 
-    @GetMapping("/inv-desc/{text}")
+    @GetMapping
+    @RequestMapping("/inv-desc/{text}")
     public Inventory[] getInventoryForProductByDesc(@PathVariable String text) {
         logger.log(this.getClass().getName());
         Inventory[] invA = new Inventory[1];
         return productService.getInventoriesDescribedWith(text).toArray(invA);
     }
 
-    @GetMapping("/orderlines/{productId}")
+    
+    @GetMapping
+    @RequestMapping("/orderlines/{productId}")
     public OrderLine[] getOrderLinesForProduct(@PathVariable String productId) {
-        logger.log(this.getClass().getName());
-        OrderLine[] orderLineDummyArray = new OrderLine[1];
+       	logger.log(this.getClass().getName());
+    	OrderLine[] orderLineDummyArray = new OrderLine[1];
         return productService.getOrderLinesForProduct(productId).toArray(orderLineDummyArray);
     }
 
-    @GetMapping("/charges/{productId}")
+    @GetMapping
+    @RequestMapping("/charges/{productId}")
     public Double getChargesForProduct(@PathVariable String productId) {
-        logger.log(this.getClass().getName());
+       	logger.log(this.getClass().getName());
         return productService.getTotalChargesForProduct(productId);
     }
 
-    @GetMapping("/all")
+    
+    @GetMapping
+    @RequestMapping("/all")
     public Product[] getProductById() {
-        logger.log(this.getClass().getName());
-        Product[] p = new Product[1];
+    	logger.log(this.getClass().getName());
+    	Product[] p = new Product[1];
         return productService.getAllProducts().toArray(p);
     }
 
-    @GetMapping("/name/{productName}")
+
+    @GetMapping
+    @RequestMapping("/name/{productName}")
     public Product getProductByName(@PathVariable String productName) {
         logger.log(this.getClass().getName());
         return productService.getProductByName(productName);
     }
 
-    @GetMapping("/desc-includes/{text}")
+    @GetMapping
+    @RequestMapping("/desc-includes/{text}")
     public Product[] getProductsDescribedBy(@PathVariable String text) {
         logger.log(this.getClass().getName());
         Product[] p = new Product[1];
         return productService.getProductsDescribedWith(text).toArray(p);
     }
 
-    @PostMapping("/register")
+    
+    @PostMapping
+    @RequestMapping("/register")
     public Product registerNewProduct(@RequestBody Product product) {
         logger.log(this.getClass().getName());
         return productService.registerProduct(product);
     }
-
-    @PostMapping("/register-list")
+    
+    @PostMapping
+    @RequestMapping("/register-list")
     public Product[] registerNewProducts(@RequestBody Product[] productArray) {
-        Product[] registeredProducts = new Product[productArray.length];
-        int i = 0;
+    	Product[] registeredProducts = new Product[productArray.length];
+    	int i = 0;
         logger.log(this.getClass().getName());
         for (Product p : productArray) {
-            registeredProducts[i] = registerNewProduct(p);
-            i++;
+        	registeredProducts[i] = registerNewProduct(p);
+        	i++;
         }
         return registeredProducts;
     }
+
+
 }
